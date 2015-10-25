2017a33d506a2b76fac72f20788c6721a2bb8963
Chris Yancey <cyancey1@gmail.com>
2015-10-25 00:09:49 -0700

    render game data via react

--- public/javascripts/game_details_react.js
+++ public/javascripts/game_details_react.js
@@ -1,20 +1,48 @@
-$(document).ready(function() {
-  console.log('whaaaa')
-
+function renderGameDetailsReactView(eventsModel) {
+  // debugger
   var GameDetails = React.createClass({
     render: function() {
       return (
-        <div className="game-details">
-          <h1>ok</h1>
+        <div id="games-container">
+          <h1>Games</h1>
+          <GameList gameData={this.props.gameData} />
         </div>
       );
     }
   })
 
+  var Game = React.createClass({
+    render: function() {
+      return(
+        <div className="game-detail">
+          <h1>{this.props.gameDetails.name}</h1>
+          <p>{this.props.gameDetails.startDateTime.toString()}</p>
+          <p>{this.props.gameDetails.timeContext}</p>
+
+        </div>
+      )
+    }
+  })
+
+  var GameList = React.createClass({
+    render: function() {
+      var gameNodes = this.props.gameData.map(function(game) {
+        return (
+          <Game gameDetails={game} />
+        )
+      })
+
+      return (
+        <div id='game-list'>
+          {gameNodes}
+        </div>
+      )
+    }
+  })
+
 
   ReactDOM.render(
-    <GameDetails />,
+    <GameDetails gameData={eventsModel.events} />,
     document.getElementById('game-details')
   );
-
-})
+}
--- public/javascripts/game_details_react_compiled_jsx.js
+++ public/javascripts/game_details_react_compiled_jsx.js
@@ -1,23 +1,65 @@
-'use strict';
+"use strict";
 
-$(document).ready(function () {
-  console.log('whaaaa');
-
+function renderGameDetailsReactView(eventsModel) {
+  // debugger
   var GameDetails = React.createClass({
-    displayName: 'GameDetails',
+    displayName: "GameDetails",
 
     render: function render() {
       return React.createElement(
-        'div',
-        { className: 'game-details' },
+        "div",
+        { id: "games-container" },
         React.createElement(
-          'h1',
+          "h1",
           null,
-          'ok'
+          "Games"
+        ),
+        React.createElement(GameList, { gameData: this.props.gameData })
+      );
+    }
+  });
+
+  var Game = React.createClass({
+    displayName: "Game",
+
+    render: function render() {
+      return React.createElement(
+        "div",
+        { className: "game-detail" },
+        React.createElement(
+          "h1",
+          null,
+          this.props.gameDetails.name
+        ),
+        React.createElement(
+          "p",
+          null,
+          this.props.gameDetails.startDateTime.toString()
+        ),
+        React.createElement(
+          "p",
+          null,
+          this.props.gameDetails.timeContext
         )
       );
     }
   });
 
-  ReactDOM.render(React.createElement(GameDetails, null), document.getElementById('game-details'));
-});
+  var GameList = React.createClass({
+    displayName: "GameList",
+
+    render: function render() {
+      var gameNodes = this.props.gameData.map(function (game) {
+        return React.createElement(Game, { gameDetails: game });
+      });
+
+      return React.createElement(
+        "div",
+        { id: "game-list" },
+        gameNodes
+      );
+    }
+  });
+
+  ReactDOM.render(React.createElement(GameDetails, { gameData: eventsModel.events }), document.getElementById('game-details'));
+}
